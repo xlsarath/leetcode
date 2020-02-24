0ab49ce6fab5b5086822fb2174526728060384f2
Makkena, Sarath <smakkena@epri.com>
2020-02-23 22:39:20 -0800

    completed

--- MergeSortedArray88.java
+++ MergeSortedArray88.java
@@ -1,20 +1,37 @@
-//time complexity : in worst case nlogn
-//space complexity : O(1)
-//leet code execution :  execution was fine, submission failure encountered
-// steps: will iterate the first array from empty spaces and fill'em with nums2. then used arrays.sort for sorting.
-import java.util.Arrays; 
+
 class MergeSortedArray88{
 
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
 
-       for(int i=m; i<nums1.length;i++){
-           nums1[i]=nums2[i-n];
-       }
-       Arrays.sort(nums1);   
-       for (int ii : nums1) {
-           System.out.println(ii);
-       }
+    //    for(int i=m; i<nums1.length;i++){
+    //        nums1[i]=nums2[i-n];
+    //    }
+    //    Arrays.sort(nums1);   
+    //    for (int ii : nums1) {
+    //        System.out.println(ii);
+    //    }
+
+    int first = m-1,second = n-1;
+    while(first>=0 && second >=0){
+        if(nums1[first]>nums2[second]){
+            nums1[first+second+1] = nums1[first];
+            first--;
+        }
+        else {
+            nums1[first+second+1] = nums2[second];
+            second --;
+        }
+
+    }
+    while(second>=0){
+        nums1[second]=nums2[second];
+        second--;
+    }
+
+    for(int i:nums1){
+        System.out.println(nums1[i-1]);
+    }
     
     }
 
