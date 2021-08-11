841a3d902693dad43a1d47c5fdc7db3665d91c95
xlsarath <xlsarathchandra@gmail.com>
2021-08-11 01:59:02 -0400

    another solution using DAG with topological sort

--- LongestPathIncreasingInMatrix_329.java
+++ LongestPathIncreasingInMatrix_329.java
@@ -2,7 +2,79 @@
 //space complexity : O(n ^ 2)
 public class LongestPathIncreasingInMatrix_329 {
 
+
+    //using Kahn's algorithm for topographical sort we have O(V + E) but here I think V = m*n and E = m*n so overall complexity is O(2*(m*n) which is O(m*n).
+     //space O(m*n)
+    int dx[] = {0, 0, 1, -1};
+    int dy[] = {1, -1, 0, 0};
     
+    public int longestIncreasingPath(int[][] matrix) {
+        
+        //base check
+        if(matrix.length == 0 ) return 0;
+        
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int inDegree[][] = new int[row][col];
+        
+        //get Indegree if neighbors are greater than current element.
+        for(int i = 0; i < row; i++ ){
+            for(int j = 0; j < col; j++){
+                for(int k = 0 ; k < 4; k++){
+                    int x = i + dx[k];
+                    int y = j + dy[k];
+                    if(isValid(row, col, x, y) && matrix[i][j] < matrix[x][y])
+                        inDegree[i][j]++;
+                }
+            }
+        }
+        
+        //add elements to queue whoose inDegree is 0
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col ; j++){
+                if(inDegree[i][j] == 0)
+                    q.offer(new int[]{i,j});
+            }
+        }
+        
+        //BFS to topological sort 
+        int length = 0;
+        while(!q.isEmpty()){
+            
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++ ){
+                
+                int []temp = q.poll();
+                
+                for(int k = 0 ; k < 4 ; k++){
+                    int x = temp[0] + dx[k];
+                    int y = temp[1] + dy[k];
+                    
+                    if(isValid(row, col, x, y) && matrix[temp[0]][temp[1]] > matrix[x][y] && --inDegree[x][y] == 0)
+                        q.offer(new int[]{x, y});
+                }
+                
+                
+            }
+            
+            
+            length++;
+        }
+        
+        return length;
+        
+        
+    }
+    
+    public boolean isValid(int row, int col, int x, int y){
+        return x >= 0 && y >= 0 && x < row && y < col;
+    }
+
+
+
+    /** 
         int dx[] = {0, 1, 0, -1};
         int dy[] = {-1, 0, 1, 0};
         
@@ -49,6 +121,6 @@ public class LongestPathIncreasingInMatrix_329 {
             return x >= 0 && y >= 0 && x < matrix.length && y < matrix[0].length ;
         }
         
-    
+    */
     
 }
