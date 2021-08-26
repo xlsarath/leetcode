e71d54d90abe1d3802476660ac55b22bf689f159
xlsarath <xlsarathchandra@gmail.com>
2021-08-26 16:20:54 -0400

    Update find-all-numbers-disappeared-in-an-array.java
    time complexity : O(n), space complexity : O(1)

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         
-        
+        /**
         HashSet<Integer> set = new HashSet<>();
         for(int num : nums) set.add(num);
         
@@ -11,6 +11,24 @@ class Solution {
                 res.add(i);
         }
         return res;
+        */
+        
+         List<Integer> result = new ArrayList<>();
+        
+        for(int i =0 ;i <nums.length; i++){
+            int index = Math.abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index] = -1 * nums[index];
+            }
+        }
+        
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]>0){
+                result.add(i+1);
+            }
+        }
+        
+        return result;
         
             
     }
