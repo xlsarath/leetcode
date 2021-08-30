b85ff749f4996b44f2b96da85458faadfb6d18d1
xlsarath <xlsarathchandra@gmail.com>
2021-08-29 22:16:34 -0400

    Update k-diff-pairs-in-an-array.java

--- k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
+++ k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.java
@@ -1,8 +1,26 @@
-// time complexity : nlogn
-//space complexity : 2*n
+//time complexity : O(n)
+//space complexity : O(n)
 class Solution {
     public int findPairs(int[] nums, int k) {
         
+         int result = 0;
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
+        
+        for(int n : nums) map.put(n, map.getOrDefault(n,0)+1);
+        
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            int key = entry.getKey();
+            int val = entry.getValue();
+            
+            if( k > 0 && map.containsKey(key + k)) result++;
+            else if(k == 0 && val > 1) result++; 
+        }
+      return result;
+        /**
+        
+        // time complexity : nlogn
+//space complexity : 2*n
         if( k < 0) return 0;
         
         HashMap<Integer,Integer> map = new HashMap<>();
@@ -36,5 +54,6 @@ class Solution {
         }
         
         return set.size();
+        */
     }
 }
