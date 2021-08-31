b6fdb04ca240b9c0b01f98566d851c870799bb5c
xlsarath <xlsarathchandra@gmail.com>
2021-08-30 23:43:04 -0400

    Update number-of-provinces.java
    updated with union find solution

--- number-of-provinces/number-of-provinces.java
+++ number-of-provinces/number-of-provinces.java
@@ -1,7 +1,50 @@
 class Solution {
     
-   
+        public int find(int parent[], int i){
+            if(parent[i] == -1)
+                return i;
+            return find(parent, parent[i]);
+        }
     
+    
+        public void union(int parent[], int x, int y){
+                
+            int rootx = find(parent,x);
+            int rooty = find(parent,y);
+            
+            if(rootx != rooty)
+                parent[rootx] = rooty;
+        }
+    
+    
+       public int findCircleNum(int[][] isConnected) {
+       
+           int parent[] = new int[isConnected.length];
+           Arrays.fill(parent, -1);
+           for(int i = 0 ; i < isConnected.length; i++){
+               for(int j = 0 ; j < isConnected.length; j++){
+                   if(isConnected[i][j] == 1 && i!=j)
+                       union(parent, i , j);
+               }
+           }
+           
+           int provinceCount = 0;
+           for(int i = 0 ; i < isConnected.length; i++)
+                if(parent[i] == -1) provinceCount++;
+           
+          return provinceCount; 
+       }
+    
+   
+}
+
+/**
+
+DFS :: 
+
+time complexity : O(n^2)
+space complexity : O(n)
+
     public int findCircleNum(int[][] isConnected) {
         
         int numOfProvinces = 0;
@@ -33,5 +76,5 @@ class Solution {
         
     }
     
-   
-}
+
+*/
