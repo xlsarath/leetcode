945d4106f06921c2bfafe7b9eeebd8aa5e01cfe4
xlsarath <xlsarathchandra@gmail.com>
2021-09-01 01:10:18 -0400

    Update graph-valid-tree.java
    added BFS and UnionFind

--- graph-valid-tree/graph-valid-tree.java
+++ graph-valid-tree/graph-valid-tree.java
@@ -1,6 +1,76 @@
+/**
+ time complexity : 
+        Iterative DFS : N + E (Nodes + Edges)
+        BFS : N + E
+        UnionFInd : O(N⋅α(N)) -- α(N) is Inverse Ackermann Function.
+        
+Space complexity for all algos: O(N)       
+
+*/
+
+class UnionFind {
+    
+    private int[] root;
+    // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+    private int[] rank;
+
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1; // The initial "rank" of each vertex is 1, because each of them is
+                         // a standalone vertex with no connection to other vertices.
+        }
+    }
+    
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+    
+    
+    public boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        
+        if(rootX == rootY) return false;
+        
+        if(rank[rootX] < rank[rootY]){
+            root[rootX] = rootY;
+            rank[rootY] += rank[rootX];
+        } else {
+            root[rootY] = rootX;
+            rank[rootX] += rank[rootY];
+        }
+        return true;
+    }
+    
+}
 class Solution {
     
     
+        public boolean validTree(int n, int[][] edges) {
+                
+            if(edges.length != n - 1) return false;
+            
+            UnionFind unionFind = new UnionFind(n);
+               
+            
+            for(int edge[] : edges ){
+                
+                int A = edge[0];
+                int B = edge[1];
+                if(!unionFind.union(A,B))
+                    return false;
+            }
+            return true;
+        }
+    
+  
+    /**
     
     public boolean validTree(int n, int[][] edges) {
         
@@ -18,7 +88,23 @@ class Solution {
             adjacencyList.get(edge[1]).add(edge[0]);
         }
         
-        dfs(0, seen, adjacencyList);
+        // dfs(0, seen, adjacencyList);
+        
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
+        seen.add(0);
+        
+        while(!q.isEmpty()){
+            
+            int node = q.poll();
+            for(int neighbor : adjacencyList.get(node)){
+                if(seen.contains(neighbor)) continue;
+                q.offer(neighbor);
+                seen.add(neighbor);
+            }
+            
+        }
+        
         
         return seen.size() == n;
     }
@@ -30,5 +116,6 @@ class Solution {
             dfs(neighbor,seen,adjacencyList);
     }
     
+    */
     
 }
