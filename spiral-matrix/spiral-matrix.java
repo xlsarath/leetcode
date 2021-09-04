2f19ae498d890f980aa1f744a6a6d44071be2c81
xlsarath <xlsarathchandra@gmail.com>
2021-09-04 03:03:22 -0400

    Time: 0 ms (100.00%), Space: 39 MB (6.81%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,45 +1,43 @@
-/**
-time complexity : O(m*n)
-space complexity : O(1)
-*/
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         
-        List<Integer> result = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
         
-        if(matrix == null || matrix.length == 0) return result;
+        if(matrix.length == 0) return res;
         
-        int top = 0, bottom = matrix.length-1, left = 0, right = matrix[0].length-1;
+        int top = 0, bottom = matrix.length-1, right = matrix[0].length-1, left = 0;
         
-        while(top <= bottom && left <= right){
+        while( top <= bottom && left <= right){
             
-            //fill left to right
-            for(int cursor = left; cursor <= right; cursor++)
-                result.add(matrix[top][cursor]);
-        
+            //left to right
+           for(int cursor = left; cursor <= right; cursor++)
+               res.add(matrix[top][cursor]);
+            
             top++;
             
-            //top to bottom
-            for(int cursor = top; cursor <= bottom; cursor++)
-                result.add(matrix[cursor][right]);
+            //right to bottom
+            for(int cursor = top; cursor <= bottom; cursor++ )
+                    res.add(matrix[cursor][right]);
             
             right--;
             
-            //right to left
+            // bottom to left
             if(top <= bottom)
                 for(int cursor = right; cursor >= left; cursor--)
-                    result.add(matrix[bottom][cursor]);
+                    res.add(matrix[bottom][cursor]);
             
             bottom--;
             
-            //bottom to top
+            //left to top
             if(left <= right)
-                for(int cursor = bottom; cursor >= top; cursor-- )
-                    result.add(matrix[cursor][left]);
-            
-            left++;
+                for(int cursor = bottom; cursor >= top ; cursor--)
+                    res.add(matrix[cursor][left]);
+             
+            left++;   
             
         }
-        return result;
+        
+        return res;
+        
     }
 }
