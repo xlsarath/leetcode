6ef3ddf2b13689e3bf1a457ac312c811b66fd97d
xlsarath <xlsarathchandra@gmail.com>
2021-09-09 13:36:27 -0400

    Time: 35 ms (42.32%), Space: 56.4 MB (17.60%) - LeetHub

--- 3sum/3sum.java
+++ 3sum/3sum.java
@@ -7,32 +7,36 @@ class Solution {
         
         Arrays.sort(nums);
         
-        for(int i = 0; i < nums.length - 2; i++){
+        for(int i = 0 ; i < nums.length - 2; i++){
             
             int low = i+1;
-            int high = nums.length -1;
-                
-            if(i > 0 && nums[i] == nums[i-1]) continue;    
+            int high = nums.length - 1;
+            
+            if(i > 0 && nums[i] == nums[i-1]) continue;
             
             while(low < high){
                 
-                int sum  = nums[i] + nums[low] + nums[high];
+                int sum = nums[i]+nums[low]+nums[high];
                 
                 if(sum == 0){
-                    result.add(Arrays.asList(nums[i],nums[low], nums[high]));
+                    result.add(Arrays.asList(nums[i],nums[low],nums[high]));
                     low++;
                     high--;
                 
                 
-                while(low < high && nums[low] == nums[low - 1]) low++;
-                while(low < high && nums[high] == nums[high+1]) high--;
+                while(low < high && nums[low] == nums[low-1]) low++;
+                while(high > low && nums[high] == nums[high+1]) high--;
                 }
-                else if(sum > 0) high--;
-                else low++;
+               else if(sum < 0){
+                    low++;
+                } else {
+                    high--;
+                }
                 
             }
-            
         }
+        
         return result;
+        
     }
 }
