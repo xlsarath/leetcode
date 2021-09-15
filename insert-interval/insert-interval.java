de1a354172bf1c5171d74e7f5f6bb52eaa4e4171
xlsarath <xlsarathchandra@gmail.com>
2021-09-14 23:15:59 -0400

    Update insert-interval.java

--- insert-interval/insert-interval.java
+++ insert-interval/insert-interval.java
@@ -1,22 +1,31 @@
+//time complexity : O(n)
+//space comolexity : O(1)
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         
         List<int[]> result = new ArrayList<>();
-                
-        for(int i = 0; i < intervals.length; i++){
+        
+        for(int i = 0 ; i < intervals.length; i++){
             
-                if(intervals[i][0] > newInterval[1] ){
-                    result.add(newInterval);
-                    newInterval = intervals[i];
-                } else if( intervals[i][1] >= newInterval[0]) {
-                    newInterval = new int[]{Math.min(newInterval[0], intervals[i][0]), 
-                                        Math.max(newInterval[1], intervals[i][1])};
-                } else {
-                    result.add(intervals[i]);
-                }
-           
+            //when to add newInterval to result list ?? [when we cross the expanded Interval]
+            if(intervals[i][0] > newInterval[1]){
+                result.add(newInterval); //[1,5]
+                newInterval = intervals[i]; //[6,9] 
+            }
+            
+            //expanding interval
+            if(intervals[i][1] >= newInterval[0]) { //[1,5]
+                newInterval = new int[]{Math.min(intervals[i][0], newInterval[0]),
+                                       Math.max(intervals[i][1], newInterval[1])};
+            } else {
+                result.add(intervals[i]);
+            }
+
         }
-        result.add(newInterval);
-            return result.toArray(new int[result.size()][]); 
+        
+        //left over interval
+        result.add(newInterval);//[6,9]
+    return result.toArray(new int[result.size()][]);        
+        
     }
 }
