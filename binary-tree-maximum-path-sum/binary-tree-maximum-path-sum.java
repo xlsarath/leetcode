999ea3aff96b04d8f6c6561719c6a36a13d75617
xlsarath <xlsarathchandra@gmail.com>
2021-09-15 21:14:32 -0400

    Time: 1 ms (37.47%), Space: 45.6 MB (6.95%) - LeetHub

--- binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -12,27 +12,26 @@
  *         this.right = right;
  *     }
  * }
+ 
+       
  */
-//time complexity : O(n)
-//space complexity : O(H) -- height of tree
 class Solution {
-    
-    Integer res;
-    
+    Integer result = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        res = Integer.MIN_VALUE;
+        
         if(root == null) return 0;
         dfs(root);
-        return res;
+        return result;
     }
     
-    
     public int dfs(TreeNode node){
-        if(node == null) return 0;
+         if(node == null) return 0;
         int left = Math.max(0, dfs(node.left));
         int right = Math.max(0, dfs(node.right));
         int sum = left + right + node.val;
-        res = Math.max(res, sum);
+        result = Math.max(result, sum);
         return Math.max(left,right) + node.val;
+        
+        
     }
 }
