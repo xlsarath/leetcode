59883db298924673fe7ef1b7cba793aeffca4b79
xlsarath <xlsarathchandra@gmail.com>
2021-09-30 18:16:53 -0400

    Time: 0 ms (100.00%), Space: 37.1 MB (74.17%) - LeetHub

--- spiral-matrix/spiral-matrix.java
+++ spiral-matrix/spiral-matrix.java
@@ -1,46 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         
+        
+        if(matrix == null || matrix[0].length == 0) return new ArrayList<>();
+        
+        int top = 0, bottom = matrix.length -1 , left = 0, right = matrix[0].length - 1;
         List<Integer> result = new ArrayList<>();
         
-        if(matrix == null || matrix[0].length == 0) return result;
-        
-        int top = 0, bottom = matrix.length - 1, left = 0, right = matrix[0].length - 1;
-        
-         
-           
-        while(left <= right && top <= bottom){
-           
+        while(top <= bottom && left <= right){
+            
             //left to right
-            for(int cursor = left; cursor <= right; cursor++)
+            for(int cursor = left ; cursor <= right; cursor++)
                 result.add(matrix[top][cursor]);
             
             top++;
-        
             //top to bottom
-            for(int cursor = top ; cursor <= bottom; cursor++)
+            for(int cursor = top; cursor <= bottom; cursor++)
                 result.add(matrix[cursor][right]);
             
-            right--;
             
+            right--;
             //right to left
             if(top <= bottom)
                 for(int cursor = right; cursor >= left; cursor--)
                     result.add(matrix[bottom][cursor]);
             
-            
             bottom--;
-            
             //bottom to top
             if(left <= right)
-                for(int cursor = bottom; cursor >= top; cursor-- )
+                for(int cursor = bottom; cursor >= top; cursor--)
                     result.add(matrix[cursor][left]);
             
-            
             left++;
+            
         }
         
         return result;
-        
     }
 }
