b9c406e599ae3bddff485094d2eba669de60927a
xlsarath <xlsarathchandra@gmail.com>
2021-10-03 17:05:47 -0400

    Time: 45 ms (99.11%), Space: 116.9 MB (89.62%) - LeetHub

--- lfu-cache/lfu-cache.java
+++ lfu-cache/lfu-cache.java
@@ -1,59 +1,10 @@
 class LFUCache {
     
-    class DLList{
-        
-        Node head, tail;
-        int size;
-            
-        DLList(){
-            
-            head = new Node(0,0);
-            tail = new Node(0,0);
-            
-            head.next = tail;
-            tail.prev = head;
-            
-            this.size = size;
-        }
-        
-        public void addFirst(Node node){
-            
-            Node headRef = head.next;
-            
-            head.next = node;
-            node.prev = head;
-            
-            node.next = headRef;
-            headRef.prev = node;
-            size++;
-            
-        }
-        
-        public void remove(Node node){
-            Node nextNode = node.next;
-            Node prevNode = node.prev;
-            
-            nextNode.prev = prevNode;
-            prevNode.next = nextNode;
-            size--;
-        }
-        
-        
-        public Node removeLast(){
-
-            Node tailNode = tail.prev;
-            remove(tailNode);
-            return tailNode;
-        }
-        
-    }
-    
-    
-    
     class Node{
         
-        Node next, prev;
-        int key, val, frequency;
+        Node next,prev;
+        int key, val;
+        int frequency;
         
         Node(int key, int val){
             this.key = key;
@@ -61,35 +12,78 @@ class LFUCache {
             this.frequency = 1;
         }
     }
-
+    
+    class DLList{
+        
+        Node head, tail;
+        int size;
+        
+        DLList(){
+            head = new Node(0,0);
+            tail = new Node(0,0);
+            
+            head.next =tail;
+            tail.prev = head;
+        }
+        
+        public void insertFirst(Node node){
+            
+            Node headRef = head.next;
+            
+            node.next = headRef;
+            head.next = node;
+            
+            node.prev = head;
+            headRef.prev = node;
+            size++;
+        }
+        
+        public Node removeLast(){
+            Node removed = tail.prev;
+            remove(removed);
+            return removed;
+        }
+        
+        public void remove(Node node){
+            
+            Node prevNode = node.prev;
+            Node nextNode = node.next;
+            
+            prevNode.next = nextNode;
+            nextNode.prev = prevNode;
+            size--;
+        }
+        
+    }
+    
     HashMap<Integer,Node> map;
     HashMap<Integer, DLList> freqMap;
-    int min;
-    int capacity;
     int size;
-    
+    int capacity;
+    int min;
+
     public LFUCache(int capacity) {
-        
         map = new HashMap<>();
-        this.capacity = capacity;
-        min = Integer.MAX_VALUE;
-        size = 0;
         freqMap = new HashMap<>();
+        size = 0;
+        this.capacity = capacity;
+        min = Integer.MIN_VALUE;
     }
     
     public int get(int key) {
+        
         Node node = map.get(key);
+        
         if(node != null){
             update(node);
             return node.val;
         }
+            
         return -1;
     }
     
     public void put(int key, int value) {
-        
         if(capacity == 0)return;
-        
         Node node = map.get(key);
         
         if(node != null){
@@ -98,7 +92,7 @@ class LFUCache {
             return;
         }
         
-        node = new Node(key,value);
+        node = new Node(key, value);
         if(size == capacity){
             DLList priorList = freqMap.get(min);
             Node removed = priorList.removeLast();
@@ -111,32 +105,29 @@ class LFUCache {
             newList = new DLList();
             freqMap.put(1, newList);
         }
-        
-        newList.addFirst(node);
-        map.put(key,node);
+        newList.insertFirst(node);
+        map.put(node.key, node);
         min = 1;
         size++;
-          
     }
-    
     
     public void update(Node node){
         
         DLList priorList = freqMap.get(node.frequency);
         priorList.remove(node);
         
-        if(priorList.size==0){
-            if(node.frequency == min){
+        if(priorList.size == 0){
+            if(min == node.frequency)
                 min++;
-            }
         }
         node.frequency++;
         DLList newList = freqMap.get(node.frequency);
         if(newList == null){
             newList = new DLList();
-            freqMap.put(node.frequency,newList);
+            freqMap.put(node.frequency, newList);
         }
-        newList.addFirst(node);
+        newList.insertFirst(node);
+        
     }
 }
 
