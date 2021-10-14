a04f6aae9b49c861c35b5240c3b0b89b2943d1ea
Sarath Makkena <xlsarathchandra@gmail.com>
2021-10-14 01:53:24 -0400

    Update subsets-ii.java
    Both BFS and BackTracking

--- subsets-ii/subsets-ii.java
+++ subsets-ii/subsets-ii.java
@@ -1,5 +1,42 @@
+/** [1,2,2]
+// []
+// [] [1]
+// [] [1] [2] [1 2]
+//.duplicate found, copy lists from [2] , skip prev list
+// [] [1] [2] [1 2] [2 2] [1 2 2]
+
+Time complexity : O(n*2^n)
+Space complexity : O(n)
+
+**/
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+         
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) return result;
+        
+        Arrays.sort(nums);
+        result.add(new ArrayList<>());
+         int size = 0;
+        for(int i = 0 ; i < nums.length; i++){
+            
+            int end = 0;
+            if( i != 0 && nums[i] == nums[i-1])    
+                    end = size;
+            size = result.size();
+            for(int j = end ; j < size; j++){
+                List<Integer> temp = new ArrayList<>(result.get(j));
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+        }
+        
+        return result;
+    }
+}
+
+/**
+ public List<List<Integer>> subsetsWithDup(int[] nums) {
         
         List<List<Integer>> result = new ArrayList<>();
         if(nums == null || nums.length == 0) return result;
@@ -27,4 +64,4 @@ class Solution {
         }
         
     }
-}
+*/
