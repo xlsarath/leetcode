92366277a982e7a0580102683a45199894a242f9
Sarath Makkena <xlsarathchandra@gmail.com>
2021-10-21 14:06:10 -0400

    Time: 37 ms (13.66%), Space: 58.2 MB (6.07%) - LeetHub

--- group-anagrams/group-anagrams.java
+++ group-anagrams/group-anagrams.java
@@ -1,23 +1,21 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        
+        
         if(strs == null || strs.length == 0) return new ArrayList<>();
         
-        HashMap<String, List<String>> map = new HashMap<>();
+        HashMap<String,List<String>> map = new HashMap<>();
         
-        
-        for(String str : strs){
-        int chars[] = new int[26];
-        for(char ch: str.toCharArray()) chars[ch-'a']++;
-            
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0 ; i < 26; i++) 
-        {   
-            sb.append("*");
-            sb.append(""+chars[i]);
-        }    
-         
-        map.putIfAbsent(sb.toString(), new ArrayList<>());
-        map.get(sb.toString()).add(str);    
+        for(String str : strs ){
+            int ch[] = new int[26];
+            for(char c : str.toCharArray()) ch[c-'a']++;
+            StringBuilder sb = new StringBuilder("*");
+            for(int i = 0 ; i < 26; i++){
+                sb.append("*");
+                sb.append(ch[i]);
+            }
+            map.putIfAbsent(sb.toString(), new ArrayList<>());
+            map.get(sb.toString()).add(str);
         }
         return new ArrayList<>(map.values());
     }
